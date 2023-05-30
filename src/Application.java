7cd030ade9d5c2912399df04887db5a443a07449
Smorchkoff <extazyice2002@gmail.com>
2023-05-31 01:02:22 +0500

    daslkjdsa

--- src/Application.java
+++ src/Application.java
@@ -1,10 +1,27 @@
+import javax.xml.transform.Result;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Application {
+    static String url = "jdbc:mysql://localhost:3306/userdb";
+    static String user = "root";
+    static String pass = "root";
     public static void main(String[] args) {
         Cell c = new Cell();
         c.fillField();
         c.viewField();
+        String query = "SELECT * FROM userdb.users";
+        try {
+            Connection conn = DriverManager.getConnection(url,user, pass);
+            Statement stmt = conn.createStatement();
+
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                System.out.println(rs.getString(2));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void input() {
@@ -13,6 +30,7 @@ public class Application {
         System.out.println("Введите свой никнейм: ");
         UserNickname = sc.nextLine();
         User u = new User(UserNickname);
+        String query;
     }
 
     //TODO Сделать реализацию выстрела
